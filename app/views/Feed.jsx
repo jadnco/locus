c477a58bf9a65db1bfab59592c827f0a665c2d64
Jaden Dessureault <jaden.dessureault@gmail.com>
2016-02-20 22:09:03 -0600

    Remove some test code from date formatter

--- app/views/Feed.jsx
+++ app/views/Feed.jsx
@@ -37,9 +37,6 @@ class Feed extends Component {
   constructor(props: Props): void {
     super(props);
 
-    let time = '2016-02-18T15:55:14.876Z';
-    let time2 = new Date();
-
     this.state = {
       spots: [],
       loading: true,
